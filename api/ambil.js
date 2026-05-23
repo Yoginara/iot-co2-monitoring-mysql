b3d27a1cbae9e8d317b4826a48c85d3e08556f4e
Yoginara <yoginara2004@gmail.com>
2026-05-23 10:17:10 +0700

    aman tanpa password

--- api/ambil.js
+++ api/ambil.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
         host: 'mysql-bce409f-yozy.f.aivencloud.com',
         port: 10252,
         user: 'avnadmin',
-        password: 'AVNS_Qo4DxGJOMwpePdX2PD',
+        password: process.env.MYSQL_PASSWORD, // 👈 WAJIB PAKAI INI, BRO! Jangan diketik teks aslinya
         database: 'defaultdb',
         ssl: { rejectUnauthorized: false }
     };
